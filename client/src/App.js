ee5f03ed2ea4ec6574313482654d5d13dea26f8c
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-11 19:16:37 +0000

    adding letters and word

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 function App() {
+  const [letter, setLetter] = useState("");
+  const [word, setWord] = useState([]);
+  const [click, setClick] = useState(0);
+  const [wordGrid, setWordGrid] = useState([]);
+
+  useEffect(() => {
+    letter && word.length < 5 && setWord(word.concat(letter));
+    word.length >= 5 && setWord([]);
+  }, [click]);
+
+  useEffect(() => {
+    click === 5 && wordGrid.length < 6 && setWordGrid([...wordGrid, word]);
+  }, [word]);
+
+  console.log("word:", word);
+  console.log("grid:", wordGrid);
+  console.log(click);
+
+  function getLetter(e) {
+    setLetter(e.target.value);
+    word.length < 5 ? setClick(() => click + 1) : setClick(0);
+  }
+
   return (
     <div className="App">
       <Header />
-      <Board />
-      <Keyboard />
+      <Board wordGrid={wordGrid} letter={letter} word={word} />
+      <Keyboard onclick={getLetter} />
     </div>
   );
 }
