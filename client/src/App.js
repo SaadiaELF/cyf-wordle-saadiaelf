bd9a0bbcf5e508125b63a4ff8ae6ed3c8c378267
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-11 11:45:43 +0000

    added board

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Board from "./components/Board";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Board />
     </div>
   );
 }
